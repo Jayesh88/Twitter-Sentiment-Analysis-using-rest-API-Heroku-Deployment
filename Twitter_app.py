f251d7ffa32d2bac8de32a9eed26e610f0949962
Jayesh Rohidas Kamat <61301712+Jayesh88@users.noreply.github.com>
2020-06-15 23:17:13 -0400

    Add files via upload

--- Twitter_app.py
+++ Twitter_app.py
@@ -25,10 +25,10 @@ def keyword():
     word=Twitter_Sentiment_Analysis.keyword.key(val)
     negative_positive_plot=Twitter_Sentiment_Analysis.plotting.sentiment(word)
     
-    
+    polarity=Twitter_Sentiment_Analysis.plotting.PolarityAndSubjectivity(word)
     word_cld=Twitter_Sentiment_Analysis.plotting.show_wordcloud(word['tweets'])
     
-    polarity=Twitter_Sentiment_Analysis.plotting.PolarityAndSubjectivity(word)
+    
     
     
     return render_template('Twitter.html',prediction=word['tweets'].head(),Top_Five_Tweets="Top Five Raw Tweets:",plot=word_cld,Word_cloud="Word Cloud for common words used in the Tweets",plot_polarity=polarity,polatiry_plot="Relationship between Subjectivity and Polarity",plot_sentiment=negative_positive_plot,Sentiment="Count of Positive, Negative and Neutral Tweets")
