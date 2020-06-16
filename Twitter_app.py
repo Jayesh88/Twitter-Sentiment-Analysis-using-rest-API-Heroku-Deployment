18326601b69c6011471f93e1a26da6a77f6eaf00
Jayesh Rohidas Kamat <61301712+Jayesh88@users.noreply.github.com>
2020-06-15 23:36:55 -0400

    Add files via upload

--- Twitter_app.py
+++ Twitter_app.py
@@ -18,21 +18,19 @@ def home():
     return render_template('Twitter.html')
 
 
-@app.route('/keyword',methods=['POST'])
+@app.route('/keyword',methods=['POST','GET'])
 def keyword():
     #val = request.form.get("val")
     val=request.form["keyword"]
     word=Twitter_Sentiment_Analysis.keyword.key(val)
-    negative_positive_plot=Twitter_Sentiment_Analysis.plotting.sentiment(word)
-    
     polarity=Twitter_Sentiment_Analysis.plotting.PolarityAndSubjectivity(word)
+    
     word_cld=Twitter_Sentiment_Analysis.plotting.show_wordcloud(word['tweets'])
-    
-    
-    
+    negative_positive_plot=Twitter_Sentiment_Analysis.plotting.sentiment(word)
     
     return render_template('Twitter.html',prediction=word['tweets'].head(),Top_Five_Tweets="Top Five Raw Tweets:",plot=word_cld,Word_cloud="Word Cloud for common words used in the Tweets",plot_polarity=polarity,polatiry_plot="Relationship between Subjectivity and Polarity",plot_sentiment=negative_positive_plot,Sentiment="Count of Positive, Negative and Neutral Tweets")
     
+
 
 if __name__ == "__main__":
     app.run()
